45f02fc6ecd0c9c4054be892d3e8f78b2be116f2
haguman <haguman@hotmail.com>
2019-12-28 02:25:55 +0100

    Add files via upload

---
+++ get-freespaceheartbeatSCOMAlerts.ps1
@@ -0,0 +1,11 @@
+﻿$LogicalDiskalerts = @()
+$heartbeatfailures = @()
+$alerts = @()
+$SCOMServer = "SCOM_Server.fqdn.com"
+
+New-SCOMManagementGroupConnection -ComputerName $SCOMServer
+$SCOMSession = get-SCOMManagementGroupConnection -ComputerName $SCOMServer
+$alerts = Get-SCOMAlert -ResolutionState (0..254) -SCSession $SCOMSession | select *
+$LogicalDiskalerts += $alerts | ?{$_.Name -like "*Logical Disk*Free Space*" -and $_.MonitoringObjectInMaintenanceMode -eq $false} | sort TimeRaised |select MonitoringObjectDisplayName,TimeRaised,Priority,IsMonitorAlert,MonitoringObjectHealthState,MonitoringObjectInMaintenanceMode,Description, TicketID
+$heartbeatfailures += $alerts | ? {$_.Name -like "*Heartbeat*" -and $_.MonitoringObjectInMaintenanceMode -eq $false} | sort TimeRaised |select MonitoringObjectDisplayName,TimeRaised,Priority,IsMonitorAlert,MonitoringObjectHealthState,MonitoringObjectInMaintenanceMode,Description, TicketID
+Get-SCOMManagementGroupConnection | Remove-SCOMManagementGroupConnection
